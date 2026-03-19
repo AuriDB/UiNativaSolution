e8807eab9169da29cfa5284470936829b0782759
BryanMillon <bryanmillon31@gmail.com>
2026-03-18 23:39:42 -0600

    Se agrega el dashboard de admin, ingeniero y dueño, y se agrega la estructura de las vistas

---
+++ WEB_UI/Controllers/OwnerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private IActionResult? CheckSession()
+        {
+            //if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserName")))
+            //    return RedirectToAction("Index", "Login");
+            //if (HttpContext.Session.GetString("UserRole") != "Dueno")
+            //    return RedirectToAction("Index", "Home");
+            return null;
+        }
+
+        public IActionResult Dashboard()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+
+        public IActionResult MyProperties()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+
+        public IActionResult RegisterProperty()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+
+        public IActionResult PaymentHistory()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+
+        public IActionResult BankAccount()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            var check = CheckSession();
+            if (check != null) return check;
+            return View();
+        }
+    }
+}
