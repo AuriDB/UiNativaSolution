df1344bdb2260dd9f17f065a3939b708cb02ad5a
Dionsx <168503089+Dionsx@users.noreply.github.com>
2026-03-21 09:04:18 -0600

    Agrega vista Dashboard y controller de Engineer

---
+++ WEB_UI/Controllers/EngineerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Controllers
+{
+    public class EngineerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
